efc2e4a4f86db4d65ed2d3cf54fdfcb1876336a7
thi2rj <81825780+thi2rj@users.noreply.github.com>
2022-04-02 19:16:08 -0300

    Add files via upload

---
+++ JavaScript1..js
@@ -0,0 +1,9 @@
+function Enviar() {
+
+    let nome = document.getElementById("nomeid");
+
+    if (nome.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }
+
+}
